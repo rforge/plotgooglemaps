9b86b4347305ba08ebfb79fcb28e9316451c5a5d
kilibarda <kilibarda@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-12-08 10:28:44 +0000

    classify improved
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plotgooglemaps@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/bubbleSP.R
+++ pkg/R/bubbleSP.R
@@ -17,30 +17,53 @@ bubbleSP <-
     obj = as(SP, "SpatialPointsDataFrame")
     data = obj@data
     if (NCOL(data) == 1){
-      z = data
+      z = as.numeric(data[,1])
     }else {
-      z = data[, zcol]  }
+      z = as.numeric( data[, zcol] )  }
     # avoid negative values
+    
     kkk=c(min(z),key.entries)
     
     kkk=sapply(2:length(kkk), function(i) mean( c(kkk[i],kkk[i-1]) ) )
     
-    ke<-abs(kkk) + mean(abs(kkk))   # no 0 as max for radius vecor
+    ke<-abs(kkk) + mean(abs(kkk))    # no zeros as max for radius vecor
     # creating a vector for subgroups
-    if(do.sqrt){
-      scale.level<- sqrt(ke/(max(ke)) ) }else{scale.level<-ke/(max(ke))}
+    if(do.sqrt){scale.level<- sqrt(ke/(max(ke)) ) }else{scale.level<-ke/(max(ke))}
     radius.level<-max.radius*scale.level
-    # list of radiuses for createSphereCircle
-    breakss<-factor(c(min(z),key.entries))
-    break_unique<-as.numeric(levels(breakss))
-    break_unique[length(break_unique)]<-max(z)
     
-    if(length(unique(z))==length(key.entries)){ zz=factor(z,labels=radius.level)
-                                                radius.vector<-floor(as.numeric(as.vector(zz))) 
-    }else{ 
-      zz=factor(cut(z,break_unique,include.lowest=TRUE ),labels=radius.level)
-      radius.vector<-floor(as.numeric(as.vector((zz))))
+    # new
+    if (key.entries[1] > min(z) ) {
+      breakss <- factor(  c(min(z), key.entries) )
+    } else {
+      breakss <- factor(  c(key.entries) )
     }
+    
+    break_unique <- as.numeric(levels(breakss))
+    
+    # new
+    if (break_unique[length(break_unique)] < max(z) ) {
+      break_unique[length(break_unique)] <- max(z)
+    }
+    
+    #setting of auxiliary variable				
+    breaks_used=1:(length(break_unique))
+    
+    if (length(unique(z)) == length(key.entries)) {
+      zz = factor(z, labels = radius.level)
+      radius.vector <- floor(as.numeric(as.vector(zz)))
+    }  else {
+      break_unique2=break_unique
+      break_unique2[length(break_unique2)] <- max(z)+1
+      #now find out which categories are used ...
+      #findInterval(z, break_unique2)
+      #data.frame(z, int=findInterval(z, break_unique2), classes=t(break_unique2))
+      breaks_used=sort(unique(findInterval(z, break_unique2)))
+      #NEW CODE
+      zz = factor(cut(z, break_unique, include.lowest = TRUE), labels = radius.level[breaks_used])        
+      radius.vector <- floor(as.numeric(as.vector((zz))))
+      
+    }
+    
     
     }else{
       radius.vector=radius.vector[bools]
